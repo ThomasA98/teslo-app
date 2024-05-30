ef7dc518516d919e64427d2487718cb2869bcfab
Thomas Koczan <andres.koczan@gmail.com>
2024-05-30 11:32:49 -0400

    seccion 16(30-5-24): DB

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,67 @@
+import { initialData } from "./seed";
+import prisma from '../lib/prisma'
+
+async function main() {
+
+    console.log(process.env.NODE_ENV);
+
+    if (process.env.NODE_ENV === 'production') return
+
+    await prisma.$transaction([
+        prisma.productImage.deleteMany(),
+        prisma.product.deleteMany(),
+        prisma.category.deleteMany(),
+    ])
+
+    await prisma.category.createMany({
+        data: initialData.categories.map(category => ({ name: category }))
+    })
+
+    const categoriesDB = await prisma.category.findMany()
+
+    const categories = categoriesDB.reduce((acc, { id, name }) => ({
+        ...acc,
+        [ name.toLowerCase() ]: id
+    }), {} as Record<string, string>)
+
+    await prisma.product.createMany({
+        data: initialData.products.map(({ type, images, ...restProduct }) => ({
+            ...restProduct,
+            categoryId: categories[type],
+        }))
+    })
+
+    const productsDB = await prisma.product.findMany({
+        select: {
+            id: true,
+            slug: true
+        }
+    })
+
+    // { slug: id, ...{ slug: id } }
+    const indexedProductsID = productsDB.reduce(
+        (acc, { id, slug }) => ({
+            ...acc, [slug]: id
+        }),
+        {} as Record<string, string>
+    )
+
+    // { image, id }[]
+    const imagesBySlug = initialData.products.flatMap(
+        ({ images, slug }) => images.map(
+            image => ({ image, id: indexedProductsID[slug] })
+        )
+    )
+
+    await prisma.productImage.createMany({
+        data: imagesBySlug.map(({ id, image }) => ({
+            productId: id,
+            url: image
+        }))
+    })
+
+}
+
+(() => {
+    main()
+})()
